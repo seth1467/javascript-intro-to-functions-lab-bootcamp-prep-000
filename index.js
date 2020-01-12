6aaee81393bce7199f57810608b6cb418fc02551
Seth Bedford <seth.bedford@yahoo.com>
2020-01-12 22:11:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   return "I Can't Hear You!"
-  if ("string" === 'hello'.toLowerCase())
+  if ("string" === something.toLowerCase())
 }
