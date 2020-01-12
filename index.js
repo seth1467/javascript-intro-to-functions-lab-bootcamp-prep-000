2379e88340aa3367c17f814d5ee5d1fdabad5de2
Seth Bedford <seth.bedford@yahoo.com>
2020-01-12 23:23:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ function sayHiToGrandma(string) {
 
 function sayHiToGrandma(string) {
   return "YES INDEED!"
-  if ("string" === "HELLO".toUpperCase()){}
+  if ("string" === "I can't hear you!".toUpperCase()){}
 }
